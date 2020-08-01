ccbacdd115d7babe01c05077d9c0cd8cafb52013
Leo <qik1992@163.com>
2020-08-01 13:19:29 +0800

    添加页面

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css'
-import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import 'antd/dist/antd.css';
+import {HashRouter, Route, Switch} from 'react-router-dom';
 import Main from './page/main.js';
 
-ReactDOM.render(
-    <Router basename="/platform">
-        <Switch>
-            <Route exact={true} path="/" component={Main} />
-            <Route exact={true} path="/main" component={Main} />
-            <Route exact={true} path="/main/:any" component={Main} />
-        </Switch>
-    </Router>, document.getElementById('root'));
+const routes = (
+    <HashRouter>
+            <Switch>
+                <Route exact = {true} path = "/main"  component = {Main} />
+                <Route exact = {true} path="/main/:any" component={Main} />
+            </Switch>
+    </HashRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,19 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ['style-loader','css-loader']
-            }
+            },
+            {
+				test:/\.(png|jpg|gif)$/i,
+				use:[
+					{
+						loader:'url-loader',
+						options:{
+							limit:81920
+						}
+					}
+				]
+			}
+
         ]
     }       
 }
